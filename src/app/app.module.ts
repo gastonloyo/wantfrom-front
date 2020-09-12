dacd77066d66a2e657371c27659520616727e7cb
Luana Andrada <luandrada12@live.com>
2020-09-11 22:31:50 -0300

    header y banner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { AsideComponent } from './componentes/aside/aside.component';
+import { GridProductsComponent } from './componentes/grid-products/grid-products.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import Popper from 'popper.js';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    AsideComponent,
+    GridProductsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
