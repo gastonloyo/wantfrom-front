0400abd5319153992cd6cb29f24311c150fc4463
Luana Andrada <luandrada12@live.com>
2020-10-30 13:45:42 -0300

    pantallas de inicio de sesion mobile

--- src/app/cart/components/checkout/checkout/checkout.component.ts
+++ src/app/cart/components/checkout/checkout/checkout.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Carrito } from 'src/app/cart/clases/carrito';
+import { ItemCarrito } from 'src/app/cart/clases/item-carrito';
+import { CartService } from 'src/app/cart/services/cart.service';
 
 @Component({
   selector: 'app-checkout',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout.component.scss']
 })
 export class CheckoutComponent implements OnInit {
-
-  constructor() { }
+  items: Array<ItemCarrito>;
+  totalPrice:number ;
+  totalQuantity:number;
+  carrito:Carrito;
+  constructor(private _cartService:CartService) { 
+    this.carrito=new Carrito();
+    this.items=[]
+  }
 
   ngOnInit(): void {
+
+    this._cartService.currentDataCart$.subscribe(x=>{
+      if(x)
+      {
+        this.items = x;
+        this.totalQuantity = x.length;
+         this.totalPrice = x.reduce((sum, current) => sum + (current.producto.precio * current.cantidad), 0);
+        
+      }
+    })
   }
   showInputAdress(){
     let newAdress= document.getElementById("newAdress12");
